fbb486b6cddc0b575d621b9f8fc7701919febde8
Mihaela Macari <mikaela.malanici@gmail.com>
2022-10-10 16:13:33 +0300

    added useLocalStorage custom hook

--- react-application/src/components/Card.jsx
+++ react-application/src/components/Card.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Card = ({ imgUrl, cardTitle, cardDescription }) => {
+const Card = ({ imgUrl, cardTitle, cardDescription, handleClick, inCart }) => {
   return (
     <div className="card text-bg-dark h-100" style={{ width: "18rem" }}>
       <img src={imgUrl} className="card-img-top h-100" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{cardTitle}</h5>
         <p className="card-text">{cardDescription}</p>
-        <div className="d-flex justify-content-between">
-          <Link to="/" className="btn btn-light">
-            See Product
-          </Link>
-          <Link to="/" className="btn btn-light ms-3">
-            Add to Cart
-          </Link>
-        </div>
+
+        {!inCart ? (
+          <div className="d-flex justify-content-center">
+            <button className="btn btn-light" onClick={handleClick}>
+              Add to Cart
+            </button>
+          </div>
+        ) : (
+          <div className="d-flex justify-content-center">
+            <button className="btn btn-light" onClick={handleClick}>
+              Remove from Cart
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
--- react-application/src/pages/products.jsx
+++ react-application/src/pages/products.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import Card from "../components/Card";
+import useLocalStorage from "../hooks/useLocalStorage";
 import { products } from "../mock/products";
 
+const ACTIONS = {
+  ADD_PRODUCT: "add-product",
+  CHANGE_CART_STATE: "change-cart-state",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.ADD_PRODUCT:
+      return [...state, action.item];
+    case ACTIONS.CHANGE_CART_STATE:
+      return products.map((item) => {
+        if (item.id === action.product.id) {
+          return { ...item, inCart: !item.inCart };
+        }
+        return item;
+      });
+    default:
+      throw new Error(`Unknown Action: ${action.type}`);
+  }
+};
+
 const ProductsPage = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+  const [cart, setCart] = useLocalStorage("cart", []);
+  const [inCart, setInCart] = useState();
+
+  const handleClick = (product) => {
+    dispatch({ type: ACTIONS.ADD_PRODUCT, product });
+    dispatch({ type: ACTIONS.CHANGE_CART_STATE, product });
+
+    console.log(product);
+    setCart((prevProduct) => {
+      return [...prevProduct, product];
+    });
+  };
   return (
     <div className="container col-xxl-8 px-4 py-5 ">
       <h2 className="mb-5">Products</h2>
@@ -10,7 +45,15 @@ const ProductsPage = () => {
         {products.map((product) => {
           return (
             <div key={product.id} className="col-sm-12 col-md-6 col-lg-3 mb-5">
-              <Card imgUrl={product.img} cardTitle={product.title} cardDescription={product.description} />
+              <Card
+                imgUrl={product.img}
+                cardTitle={product.title}
+                cardDescription={product.description}
+                handleClick={() => {
+                  handleClick(product);
+                }}
+                inCart={inCart}
+              />
             </div>
           );
         })}
